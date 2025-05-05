5292a5797b7877aa2283aa58d09d475eb7d248b8
shaikhanumanbabu453 <shaikhanumanbabu453@gmail.com>
2025-05-05 06:11:06 +0530

    in row 3 images logic added.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -27,7 +27,7 @@ const Dashboard = () => {
         {/* another filter */}
 
 
-        <div className="flex flex-col sm:flex-row items-center gap-4 p-4 bg-gray-100 rounded-lg">
+        <div className="flex flex-col sm:flex-row items-center gap-4 p-4 bg-gray-100 rounded-lg mt-4">
       {/* Search Box */}
       <input
         type="text"
@@ -51,8 +51,46 @@ const Dashboard = () => {
     </div>
 
         {/* cards section here */}
-        <div className="flex flex-col sm:flex-row justify-center items-stretch gap-4 p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
 
+
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <img className="rounded-t-lg" src="/images/image-1.jpg" alt="" />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <Link
+              to={`/events/1`}
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+          </div>
+        </div>
 
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
